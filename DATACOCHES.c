39855998d3b2776b0ed989d0ed89b09864608f5d
angelgomezmunoz <47790895+angelgomezmunoz@users.noreply.github.com>
2019-04-05 13:38:11 +0200

    Update DATACOCHES.c
    También se incluyes algunas líneas de código para cambiar los colores del fondo y las letras, y permitir imprimir letras en pantalla con tildes.

--- DATACOCHES.c
+++ DATACOCHES.c
@@ -12,25 +12,25 @@ typedef struct
 }ficha;
 
 int main(){
-	system("chcp 1252");//Código para que se puedan imprimir letras con tildes
-	system("cls");//Código para que no se imprima la línea anterior
-	system("COLOR F0");//Código para cambiar el color del fondo (1er dígito) y de las letras (2o digito) (0 = Negro, 1 = Azul, 2 = Verde, 3 = Aqua, 4 = Rojo, 5 = Púrpura, 6 = Amarillo, 7 = Blanco, 8 =Gris, 9 = Azul Claro, A = Luz Verde, B = Aqua Luz, C = Rojo, D = Luz Violeta, E = = Amarillo Claro, F = Blanco Brillante)
+	system("chcp 1252");//CÃ³digo para que se puedan imprimir letras con tildes
+	system("cls");//CÃ³digo para que no se imprima la lÃ­nea anterior
+	system("COLOR F0");//CÃ³digo para cambiar el color del fondo (1er dÃ­gito) y de las letras (2o digito) (0 = Negro, 1 = Azul, 2 = Verde, 3 = Aqua, 4 = Rojo, 5 = PÃºrpura, 6 = Amarillo, 7 = Blanco, 8 =Gris, 9 = Azul Claro, A = Luz Verde, B = Aqua Luz, C = Rojo, D = Luz Violeta, E = = Amarillo Claro, F = Blanco Brillante)
 	
 	int opcion;
 	
-	printf("DATACOCHES\n\n¿Qué desea hacer?\n\n");
+	printf("DATACOCHES\n\nÂ¿QuÃ© desea hacer?\n\n");
 	
 	do{
-	    printf("TECLA   ACCIÓN\n  1-    Consultar datos sobre un vehículo.\n  2-    Consultar estado de un vehículo.\n  3-    Consultar calendario.\n  4-    Generar nueva cita.\n  5-    Crear nuevo usuario.\n  0-    Salir del programa.\n\n   OPCIÓN: ");
+	    printf("TECLA   ACCIÃ“N\n  1-    Consultar datos sobre un vehÃ­culo.\n  2-    Consultar estado de un vehÃ­culo.\n  3-    Consultar calendario.\n  4-    Generar nueva cita.\n  5-    Crear nuevo usuario.\n  0-    Salir del programa.\n\n    OPCIÃ“N: ");
 	    scanf("%i" ,&opcion);
 	    printf("\n\n");
 	    switch(opcion)
 	    {
 	    	case 1:
-	    		printf("CONSULTA DE DATOS\nIntroduzca la matrícula del vehículo:");
+	    		printf("CONSULTA DE DATOS\nIntroduzca la matrÃ­cula del vehÃ­culo:");
 	    		break;
 	    	case 2:
-	    		printf("CONSULTA DE ESTADO\nIntroduzca la matrícula del vehículo:");
+	    		printf("CONSULTA DE ESTADO\nIntroduzca la matrÃ­cula del vehÃ­culo:");
 		    	break;
 	    	case 3:
 		    	printf("CALENDARIO\n\n");
@@ -48,7 +48,7 @@ int main(){
 	    		printf("Tecla no reconocida");
 		    	break;	
     	}
-    	printf("\n\n");//doble salto de línea provisional
+    	printf("\n\n");//doble salto de lÃ­nea provisional
 	    system("pause");
 	}
 	while (opcion != 0);
